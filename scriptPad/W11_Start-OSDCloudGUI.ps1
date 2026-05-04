5381c83539d01eab1bc548441cb69729dffaaff4
Gelders <42740552+Gelders@users.noreply.github.com>
2026-05-04 11:55:16 +0200

    Update W11_Start-OSDCloudGUI.ps1
    Install and import OSD module
    FIXED: problems with invoking scripts form web.

--- scriptPad/W11_Start-OSDCloudGUI.ps1
+++ scriptPad/W11_Start-OSDCloudGUI.ps1
@@ -1,1 +1,18 @@
+#================================================
+#   [PreOS] Update Module
+#================================================
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force   
+
+#=======================================================================
+#   [OS] Params and Start-OSDCloudGUI
+#=======================================================================
 Start-OSDCloudGUI -BrandName "" -BrandColor "#17458a" -OSLanguage "nl-nl" -OSVersion "Windows 11" -OSEdition "Pro" -OSActivation "Retail" -RestartComputer -UpdateDiskDrivers -UpdateNetworkDrivers -UpdateSCSIDrivers -SyncMSUpCatDriverUSB -OEMActivation -WindowsUpdate -WindowsUpdateDrivers -WindowsDefenderUpdate
